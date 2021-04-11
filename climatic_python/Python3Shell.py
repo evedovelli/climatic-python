2c5eebb6236e88644da1690dfa6ecf32ceca0052
Estevan Vedovelli <evedovelli@gmail.com>
2021-04-10 22:57:58 -0300

    Accept now any of CoreCli options on Python3Shell constructor
    Signed-off-by: Estevan Vedovelli <evedovelli@gmail.com>

--- climatic_python/Python3Shell.py
+++ climatic_python/Python3Shell.py
@@ -2,6 +2,7 @@ import pexpect
 
 from climatic.CoreCli import CoreCli
 from climatic.connections.Ssh import Ssh, PTY_WINSIZE_COLS
+from typing import List, Optional
 
 
 ####################################################################################################
@@ -11,23 +12,21 @@ class Python3Shell(CoreCli):
     """ Extend CoreCli with Python3Shell customizations.
     """
 
-    def run(self, cmds: str, **run_opts):
-        """ Execute Python3 commands
-
-        @param cmds      A multi-line string with commands to be executed.
-        @param run_opts  Same options as CoreCli run method.
+    def __init__(self, connection, **opts):
+        """ Initialize OcNOS CLI.
+        @param connection  The connection object to be used for accessing the CLI.
+        @param opts        Same options as CoreCli initializer.
         """
-        if not 'marker' in run_opts:
-            run_opts['marker'] = '>>>|...'
+        if not 'marker' in opts:
+            self.marker = '>>>|...'
+        if not 'error_marker' in opts:
+            self.error_marker = 'Error'
+        if not 'strip_cmds' in opts:
+            self.strip_cmds = False
 
-        if not 'error_marker' in run_opts:
-            run_opts['error_marker'] = 'Error'
-
-        if not 'strip_cmds' in run_opts:
-            run_opts['strip_cmds'] = False
-
-        return super(Python3Shell, self).run(cmds, **run_opts)
-
+        CoreCli.__init__(self,
+                         connection,
+                         **opts)
 
 ####################################################################################################
 ## SshPython3Shell
@@ -37,7 +36,13 @@ class SshPython3Shell(Python3Shell):
     Core implementation is done by Ssh and Python3Shell.
     """
 
-    def __init__(self, ip: str, username: str, password: str, port=22, sh_markers=['#', '>']):
+    def __init__(self,
+                 ip: str,
+                 username: str,
+                 password: str,
+                 port: Optional[int]=22,
+                 sh_markers: Optional[List[str]]=['#', '>'],
+                 **opts):
         """ Initialize Python3 Shell.
         @param ip          IP address of target. Ex: '234.168.10.12'
         @param username    username for opening SSH connection
@@ -45,11 +50,17 @@ class SshPython3Shell(Python3Shell):
         @param port        Port used for SSH connection. Defaults to 22
         @param sh_markers  List of accepted markers for the shell when the SSH is established and
                            before launching python3 interpreter
+        @param opts        Same options as CoreCli initializer.
         """
         self.sh_markers = sh_markers
         self.name = "Python3.SSH"
         ssh = Ssh(ip, username, port=port)
-        Python3Shell.__init__(self, ssh, username=username, password=password, pty_winsize_cols=PTY_WINSIZE_COLS)
+        Python3Shell.__init__(self,
+                              ssh,
+                              username=username,
+                              password=password,
+                              pty_winsize_cols=PTY_WINSIZE_COLS,
+                              **opts)
 
     def login(self):
         """ Login to CLI interface.
